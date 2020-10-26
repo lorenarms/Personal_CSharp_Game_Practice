1cf2e040046e07d449f00462ce586e4c289480fa
Lawrence Artl <lawrence.artl@gmail.com>
2020-10-26 13:06:04 -0700

    Began project

--- Game01/Game.cs
+++ Game01/Game.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using static System.Console;
+using static System.Console;                                //remove need to type Console before everythingaaaaa
 
 namespace Game01
 {
@@ -15,21 +15,21 @@ namespace Game01
             Write("X");
             */
 
-            string[,] grid =
+            string[,] grid =                                //2 dimensional array
             {
                 {"1", "2", "3" },
                 {"4", "5", "6" },
                 {"7", "8", "9" },
             };
 
-            int rows = grid.GetLength(0);
-            int cols = grid.GetLength(1);
-            for (int y = 0; y < rows; y++)
+            int rows = grid.GetLength(0);                   //(0) for rows in 2D array
+            int cols = grid.GetLength(1);                   //(1) for columns in 2D array
+            for (int y = 0; y < rows; y++)                  //loop to build map
             {
                 for (int x = 0; x < cols; x++)
                 {
-                    string element = grid[y, x];
-                    SetCursorPosition(x, y);
+                    string element = grid[y, x];            //columns vs rows
+                    SetCursorPosition(x, y);                //rows vs columns
                     Write(element);
                 }
             }
