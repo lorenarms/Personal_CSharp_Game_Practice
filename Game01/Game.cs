b83834b039b3d758703dd56ef3de8bc75c2bb898
Lawrence Artl <lawrence.artl@gmail.com>
2020-10-26 12:08:36 -0700

    Add project files.

---
+++ Game01/Game.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace Game01
+{
+    class Game
+    {
+        public void Start()
+        {
+            Console.WriteLine("The game is starting.");
+            /*
+            SetCursorPosition(4, 2);
+            Write("X");
+            */
+
+            string[,] grid =
+            {
+                {"1", "2", "3" },
+                {"4", "5", "6" },
+                {"7", "8", "9" },
+            };
+
+            int rows = grid.GetLength(0);
+            int cols = grid.GetLength(1);
+            for (int y = 0; y < rows; y++)
+            {
+                for (int x = 0; x < cols; x++)
+                {
+                    string element = grid[y, x];
+                    SetCursorPosition(x, y);
+                    Write(element);
+                }
+            }
+
+            Console.WriteLine("\n\nPress any key to exit");
+            Console.ReadKey(true);
+        }
+
+    }
+}
---
+++ Game01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Game01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game currentGame = new Game();
+            currentGame.Start();
+
+
+        }
+    }
+}
